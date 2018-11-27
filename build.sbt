da6f8cc7e7dbe78d5aaca34e3c6dce936957e344
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-28 00:39:58 +0100

    - Demo.scala added to compare with Demo.java in other project (Scala is simplier!)

--- build.sbt
+++ build.sbt
@@ -1,6 +1,4 @@
 name := "chat_with_Scala"
-
 version := "0.0.1"
-
 scalaVersion := "2.12.7"
 libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.6"
--- src/main/scala/ChatMessage.scala
+++ src/main/scala/ChatMessage.scala
@@ -1,8 +1,3 @@
-import scala.xml._
-import scala.xml
-import scala.collection.mutable.HashMap
-import scala.collection.immutable.{TreeMap, TreeSet}
-
 object ChatMessage {
   def main(args: Array[String]) {
     println("Hello, world!")
