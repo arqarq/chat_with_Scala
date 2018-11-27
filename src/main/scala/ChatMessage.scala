329d7274d10a02b8b83fcdd39faeba9aeef4cdfa
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-27 15:57:34 +0100

    - first object in Scala

---
+++ src/main/scala/ChatMessage.scala
@@ -0,0 +1,10 @@
+import scala.xml._
+import scala.xml
+import scala.collection.mutable.HashMap
+import scala.collection.immutable.{TreeMap, TreeSet}
+
+object ChatMessage {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+}
