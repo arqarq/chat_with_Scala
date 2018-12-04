d96e0e6950c7499d1962eb1370c802fba168c2d8
arqarq <43341071+arqarq@users.noreply.github.com>
2018-12-04 12:08:25 +0100

    - Demo14 - anonymous function clarified

--- src/main/scala/learn/Demo14.scala
+++ src/main/scala/learn/Demo14.scala
@@ -1,11 +1,15 @@
 package learn
 
 object Demo14 {
-  private val userDir = () => {
-    System.getenv("JAVA_HOME")
+  private val userDir = (x: Int) => {
+    System.getProperty("user.dir")
+    //    System.getenv()
   }
 
   def main(args: Array[String]) {
     println(userDir)
+    //    println(userDir.apply)
+    println(userDir.apply(1))
+    println(System.getProperty("java.home"))
   }
 }
