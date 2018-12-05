e52bf5a95c9bad1a9556513adea447bbf7354bf7
arqarq <43341071+arqarq@users.noreply.github.com>
2018-12-05 20:25:05 +0100

    - Demo 18

--- src/main/scala/learn/Demo18.scala
+++ src/main/scala/learn/Demo18.scala
@@ -3,6 +3,10 @@ package learn
 object Demo18 extends App {
   var tup: (Int, String, String, Console.type) = (1, "String", """literal""", Console)
   var tup4 = new Tuple4[Int, String, String, Console.type](1, "cos", "cos2", Console)
+  var tup3 = new Tuple4[Int, String, String, List[List[Int]]](1, "cos", "cos2",
+    (1 :: (0 :: (0 :: Nil))) :: (2 :: (3 :: (4 :: Nil))) :: Nil)
+  var tup5 = new Tuple4[Int, String, String, List[List[Int]]](1, "cos", "cos2",
+    List(List(1, 0, 0), List(0, 1, 0), List(0, 0, 1)))
   var tup2 = Tuple2(2, 4)
 
   println(tup.toString())
@@ -10,5 +14,10 @@ object Demo18 extends App {
   println(tup._2.concat(tup._3))
   println(tup._4.flush())
   println(tup4._2.concat(tup4._3))
+  println(tup2)
+  tup2 = tup2.swap
+  println(tup2)
   println(tup2._1 + tup2._2)
+  println(tup5._4.+(" qqq").+(3))
+  println(tup5._4 + " qqq" + 3)
 }
