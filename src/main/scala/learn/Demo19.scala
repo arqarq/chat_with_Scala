72889f5fce46917b29aa02aaa044b2350d75d340
arqarq <43341071+arqarq@users.noreply.github.com>
2018-12-06 11:17:26 +0100

    - Demo19

--- src/main/scala/learn/Demo19.scala
+++ src/main/scala/learn/Demo19.scala
@@ -12,11 +12,20 @@ object Demo19 {
     val additional = new Additional
     println(additional.twos)
     println(additional.fruit)
+    println(additional.squares)
+    println(additional.mul)
+    println(additional.mul2)
   }
 
   class Additional {
     val twos: List[Int] = List.fill(10)(2)
     val fruit: List[String] = List.fill(3)("apple")
+
+    val squares: List[Int] = List.tabulate[Int](6)(n => n * n)
+    val mul: List[List[Int]] = List.tabulate[Int](5, 6)(_ * _)
+    val mul2: List[List[Int]] = List.tabulate(5, 6)(f)
+
+    private def f(n1: Int, n2: Int): Int = n1 * n2
   }
 
 }
