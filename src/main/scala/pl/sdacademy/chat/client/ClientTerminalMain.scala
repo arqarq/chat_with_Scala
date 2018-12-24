0ccb14f447423bc23cd39bb1a5540f135e69a0da
arqarq <43341071+arqarq@users.noreply.github.com>
2018-12-24 01:21:47 +0100

    - ClientTerminalMain continued

--- src/main/scala/pl/sdacademy/chat/client/ClientTerminalMain.scala
+++ src/main/scala/pl/sdacademy/chat/client/ClientTerminalMain.scala
@@ -1,6 +1,9 @@
 package pl.sdacademy.chat.client
 
 object ClientTerminalMain {
+  val timeBetweenReconnectAttempt = 5
+  var reconnectionAttemps = 3
+
   @throws(classOf[InterruptedException])
   def main(args: Array[String]): Unit = {
     val thread = new ClientTerminal
