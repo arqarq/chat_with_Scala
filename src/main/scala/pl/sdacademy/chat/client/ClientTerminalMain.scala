5bc41038c4c3681c98cf332938f1486bad3aa251
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-27 22:30:24 +0100

    - ClientTerminalMain finished

--- src/main/scala/pl/sdacademy/chat/client/ClientTerminalMain.scala
+++ src/main/scala/pl/sdacademy/chat/client/ClientTerminalMain.scala
@@ -1,14 +1,43 @@
 package pl.sdacademy.chat.client
 
+import java.io.IOException
+
 object ClientTerminalMain {
   val timeBetweenReconnectAttempt = 5
   var reconnectionAttemps = 3
 
   @throws(classOf[InterruptedException])
   def main(args: Array[String]): Unit = {
-    val thread = new ClientTerminal
-    thread.setName("Terminal")
-    thread.start()
-    thread.join()
+    println("Welcome to Awesome Chat!")
+    while (reconnectionAttemps > 0) {
+      try {
+        println("Attempting to connect to server")
+        val clientTerminal = new ClientTerminal
+        val thread = new Thread(clientTerminal)
+        thread.setName("Terminal")
+        thread.start()
+        thread.join()
+        return
+      } catch {
+        case ex: IOException =>
+          println("Could not connect to server: " + ex.getMessage)
+          reconnectionAttemps = reconnectionAttemps - 1
+          if (reconnectionAttemps > 0) {
+            countdown(timeBetweenReconnectAttempt)
+          }
+      }
+    }
+    println("See you soon!")
+  }
+
+  @throws(classOf[InterruptedException])
+  private def countdown(a: Int): Unit = {
+    var i: Int = a
+
+    while (i > 0) {
+      println("### Waiting " + i + " till reconnect attempt...")
+      Thread.sleep(1000)
+      i = i - 1
+    }
   }
 }
